c942a96eb333f255e4135b2becfc5b5836d067d4
Martin Betz <martinmichaelbetz@gmail.com>
2023-12-10 12:15:32 +0100

    Initial commit

---
+++ split.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Input markdown file
+input_file="in.md"
+
+# Check if input file exists
+if [ ! -f "$input_file" ]; then
+    echo "Input file $input_file does not exist."
+    exit 1
+fi
+
+# Read the input markdown file and store its content
+quicknote=$(<"$input_file")
+
+# Split the quicknote by ---- and create files
+blocks=$(echo "$quicknote" | awk '/----/ {p=1;next} p')
+while read -r block; do
+    if [[ ! -z "$block" ]]; then
+        timestamp=$(date '+%y%m%d%H%M')
+        content=$(echo "$block" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+        title=$(echo "$content" | head -n1 | awk '{print $1" "$2" "$3}' | sed 's/[^a-zA-Z0-9_ ]//g' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | tr '[:upper:]' '[:lower:]')
+        echo "$content" > "$timestamp-$title.md"
+    fi
+done <<< "$blocks"
